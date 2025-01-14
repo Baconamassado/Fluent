cc358ec8d9665095f4006fcf9520d8139f954ba7
Bacon <149910312+Baconamassado@users.noreply.github.com>
2025-01-13 21:45:33 -0300

    Update Red.lua

--- src/Themes/Red.lua
+++ src/Themes/Red.lua
@@ -1,47 +1,48 @@
 return {
-Accent = Color3.fromRGB(255, 0, 0), -- Vermelho intenso
+    Name = "Red",
+    Accent = Color3.fromRGB(255, 0, 0), -- Vermelho intenso
 
-AcrylicMain = Color3.fromRGB(20, 20, 20),
-AcrylicBorder = Color3.fromRGB(139, 69, 69), -- Vermelho acinzentado
-AcrylicGradient = ColorSequence.new(Color3.fromRGB(85, 0, 0), Color3.fromRGB(40, 0, 0)), -- De um vermelho escuro para um vermelho ainda mais escuro
-AcrylicNoise = 0.92,
+    AcrylicMain = Color3.fromRGB(20, 20, 20),
+    AcrylicBorder = Color3.fromRGB(139, 69, 69), -- Vermelho acinzentado
+    AcrylicGradient = ColorSequence.new(Color3.fromRGB(85, 0, 0), Color3.fromRGB(40, 0, 0)), -- De um vermelho escuro para um vermelho ainda mais escuro
+    AcrylicNoise = 0.92,
 
-TitleBarLine = Color3.fromRGB(139, 69, 69), -- Vermelho acinzentado
-Tab = Color3.fromRGB(160, 50, 50), -- Vermelho escuro acinzentado
+    TitleBarLine = Color3.fromRGB(139, 69, 69), -- Vermelho acinzentado
+    Tab = Color3.fromRGB(160, 50, 50), -- Vermelho escuro acinzentado
 
-Element = Color3.fromRGB(255, 102, 102), -- Vermelho claro escuro
-ElementBorder = Color3.fromRGB(0, 0, 0), -- Preto
-InElementBorder = Color3.fromRGB(139, 0, 0), -- Vermelho escuro
-ElementTransparency = 0.87,
+    Element = Color3.fromRGB(255, 102, 102), -- Vermelho claro escuro
+    ElementBorder = Color3.fromRGB(0, 0, 0), -- Preto
+    InElementBorder = Color3.fromRGB(139, 0, 0), -- Vermelho escuro
+    ElementTransparency = 0.87,
 
-ToggleSlider = Color3.fromRGB(255, 102, 102), -- Vermelho claro escuro
-ToggleToggled = Color3.fromRGB(0, 0, 0), -- Preto
+    ToggleSlider = Color3.fromRGB(255, 102, 102), -- Vermelho claro escuro
+    ToggleToggled = Color3.fromRGB(0, 0, 0), -- Preto
 
-SliderRail = Color3.fromRGB(255, 102, 102), -- Vermelho claro escuro
+    SliderRail = Color3.fromRGB(255, 102, 102), -- Vermelho claro escuro
 
-DropdownFrame = Color3.fromRGB(255, 153, 153), -- Vermelho claro
-DropdownHolder = Color3.fromRGB(0, 0, 0), -- Preto
-DropdownBorder = Color3.fromRGB(139, 69, 69), -- Vermelho escuro acinzentado
-DropdownOption = Color3.fromRGB(0, 0, 0), -- Preto
+    DropdownFrame = Color3.fromRGB(255, 153, 153), -- Vermelho claro
+    DropdownHolder = Color3.fromRGB(0, 0, 0), -- Preto
+    DropdownBorder = Color3.fromRGB(139, 69, 69), -- Vermelho escuro acinzentado
+    DropdownOption = Color3.fromRGB(0, 0, 0), -- Preto
 
-Keybind = Color3.fromRGB(139, 0, 0), -- Vermelho escuro
+    Keybind = Color3.fromRGB(139, 0, 0), -- Vermelho escuro
 
-Input = Color3.fromRGB(139, 0, 0), -- Vermelho escuro
-InputFocused = Color3.fromRGB(40, 0, 0), -- Vermelho muito escuro
-InputIndicator = Color3.fromRGB(255, 153, 153), -- Vermelho claro
+    Input = Color3.fromRGB(139, 0, 0), -- Vermelho escuro
+    InputFocused = Color3.fromRGB(40, 0, 0), -- Vermelho muito escuro
+    InputIndicator = Color3.fromRGB(255, 153, 153), -- Vermelho claro
 
-Dialog = Color3.fromRGB(40, 0, 0), -- Vermelho muito escuro
-DialogHolder = Color3.fromRGB(70, 0, 0), -- Vermelho profundo
-DialogHolderLine = Color3.fromRGB(0, 0, 0), -- Preto
-DialogButton = Color3.fromRGB(40, 0, 0), -- Vermelho muito escuro
-DialogButtonBorder = Color3.fromRGB(0, 0, 0), -- Preto
-DialogBorder = Color3.fromRGB(0, 0, 0), -- Preto
-DialogInput = Color3.fromRGB(70, 0, 0), -- Vermelho profundo
-DialogInputLine = Color3.fromRGB(0, 0, 0), -- Preto
+    Dialog = Color3.fromRGB(40, 0, 0), -- Vermelho muito escuro
+    DialogHolder = Color3.fromRGB(70, 0, 0), -- Vermelho profundo
+    DialogHolderLine = Color3.fromRGB(0, 0, 0), -- Preto
+    DialogButton = Color3.fromRGB(40, 0, 0), -- Vermelho muito escuro
+    DialogButtonBorder = Color3.fromRGB(0, 0, 0), -- Preto
+    DialogBorder = Color3.fromRGB(0, 0, 0), -- Preto
+    DialogInput = Color3.fromRGB(70, 0, 0), -- Vermelho profundo
+    DialogInputLine = Color3.fromRGB(0, 0, 0), -- Preto
 
-Text = Color3.fromRGB(70, 0, 0), -- Vermelho profundo
-SubText = Color3.fromRGB(0, 0, 0), -- Preto
+    Text = Color3.fromRGB(70, 0, 0), -- Vermelho profundo
+    SubText = Color3.fromRGB(0, 0, 0), -- Preto
 
-Hover = Color3.fromRGB(255, 102, 102), -- Vermelho claro escuro
-HoverChange = 0.04
+    Hover = Color3.fromRGB(255, 102, 102), -- Vermelho claro escuro
+    HoverChange = 0.04
 }
